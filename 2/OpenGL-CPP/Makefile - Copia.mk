0f4d5d0369a881a70efc2ecf3d92ba5fc58668e6
Humberto Corrêa Gomes <humberto.gomes002@edu.pucrs.br>
2025-09-06 14:11:37 -0300

    Move OpenGL-CPP project from M2 to 2 directory
    All files and folders from M2/OpenGL-CPP were moved to 2/OpenGL-CPP, including source code, images, configuration, and project files. Redundant T1 directory files were deleted, consolidating the project structure under the new location.

--- 2/OpenGL-CPP/Makefile - Copia.mk
+++ 2/OpenGL-CPP/Makefile - Copia.mk
@@ -2,7 +2,7 @@
 
 PROG = BasicoOpenGL.exe
 #FONTES = Linha.cpp Ponto.cpp Temporizador.cpp InterseccaoEntreTodasAsLinhas.cpp 
-FONTES = Ponto.cpp Poligono.cpp Temporizador.cpp ListaDeCoresRGB.cpp Instancia.cpp ModeloMatricial.cpp TransformacoesGeometricas.cpp
+FONTES = Ponto.cpp Poligono.cpp Temporizador.cpp ListaDeCoresRGB.cpp ProgramaBasicoOpenGL1.cpp
 OBJETOS = $(FONTES:.cpp=.cpp)
 CFLAGS = -g -Iinclude\GL # -Wall -g  # Todas as warnings, infos de debug
 #LDFLAGS = -Llib\GL -lfreeglut -lopengl32 -lglu32 -lm
--- 2/OpenGL-CPP/Makefile.mk
+++ 2/OpenGL-CPP/Makefile.mk
@@ -9,7 +9,7 @@ FONTES = Ponto.cpp Poligono.cpp Temporizador.cpp ListaDeCoresRGB.cpp ProgramaBas
 
 OBJETOS = $(FONTES:.cpp=.cpp)
 
-CFLAGS = -g -Iinclude  -Wno-write-strings -Wno-narrowing # -Wall -g  # Todas as warnings, infos de debug
+CFLAGS = -g -Iinclude\GL  -Wno-write-strings -Wno-narrowing # -Wall -g  # Todas as warnings, infos de debug
 #LDFLAGS = -Llib\GL -lfreeglut -lopengl32 -lglu32 -lm
 LDFLAGS = -Llib -lfreeglut -lopengl32 -lglu32 -lm
 CC = g++
